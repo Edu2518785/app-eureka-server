13a3b5d2b038416840ea14d14241d2d4309a0298
Edu2518785 <diego.huayra.jaramillo@gmail.com>
2025-02-23 14:53:53 -0500

    eureka server terminado

---
+++ src/test/java/pe/idat/edu/app_eureka_server/AppEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.app_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
